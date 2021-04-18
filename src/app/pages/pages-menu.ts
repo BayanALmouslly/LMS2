0c627970a64fc28e3ecba638b2f689b0a90ceed4
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-04-18 05:48:55 +0300

    start page

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -4,7 +4,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'الصفحة الرئيسية',
     icon: 'shopping-cart-outline',
-    link: '/pages/homepage',
+    link: '/pages/homepage/start',
     home: true,
   },
   
